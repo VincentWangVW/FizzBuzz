075f5c0348ba2bcdcd0d35c9a2cb388263cd6cdf
Vincent <vincentwangvw05@gmail.com>
2024-09-09 16:20:53 -0400

    Added Reduce which divedes a number by 2 if its even and subtracts 1 if its odd and returns the steps

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int start = 100;
+        int steps = 0;
+        while (start != 0) {
+            if (start % 2 == 0) {
+                start = start / 2;
+            } else {
+                start = start - 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
 }
